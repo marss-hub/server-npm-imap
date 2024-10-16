0f2e562b018022e24c393a5c1b320a971d2ae2ea
Svetlana <svetlana@it-record.ru>
2024-10-16 01:02:52 +0000

    did some tests and refactoring more

--- index.mjs
+++ index.mjs
@@ -1,5 +1,4 @@
 import {simpleParser} from 'mailparser';
-import _ from 'lodash';
 import { CONFIGDATA } from "./configData.mjs";
 import * as imapSimple from 'imap-simple';
 
@@ -17,10 +16,16 @@ let config = {
 
 
 const connection = await imapSimple.connect(config);
+
+    // Returns the full list of mailboxes (folders).
+    // const boxesList = await connection.getBoxes()
+    // console.log('boxesList', boxesList);
+
+
     await connection.openBox('INBOX');
     // See more https://www.php.net/manual/ru/function.imap-search.php about criteria parameter
     // See more how it IRL write and work https://stackoverflow.com/questions/57557330/how-to-fetch-email-thread-by-messageid-using-imap-in-nodejs
-    const searchCriteria = ['UNSEEN']; // [['SUBJECT', 'test msg']];
+    const searchCriteria = ['UNSEEN']; // ['ALL'] , [['SUBJECT', 'test msg']];
     const fetchOptions = {
         //A string or Array of strings containing the body part section to fetch. See more https://github.com/mscdex/node-imap
         bodies: [''], //  The entire message (header + body)
@@ -29,24 +34,31 @@ const connection = await imapSimple.connect(config);
     //console.log("messages : ", messages) // can be empty arr
     //console.log("messages : ", messages[0].parts)
 
-    messages.forEach(function (item) {
-        const all = _.find(item.parts, { "which": "" });
+    let messageId = null // id for move only
+    messages.forEach(async function (item) {
+        messageId = item.seqNo;
+        const all = item.parts.find(item => item.which === "");
+
         const id = item.attributes.uid;
         const idHeader = "Imap-Id: " + id + "\r\n";
-        simpleParser(idHeader + all.body, (err, mail) => {
-            // access to the whole mail object
-            console.log(`\n=================================================\n`);
-            //console.log(mail)
-            console.log('mail.from.value.address: ', mail.from.value[0].address);
-            console.log('mail.subject: ', mail.subject);
-            console.log('mail.messageId: ', mail.messageId);
-            console.log('mail.text: ', mail?.text) 
-            console.log('id/attributes.uid : ', id);
-        });
+        const mail = await simpleParser(idHeader + all.body);
+        // access to the whole mail object
+        console.log(`\n=================================================\n`);
+        //console.log(mail)
+        console.log('mail.from.value.address: ', mail.from.value[0].address);
+        console.log('mail.subject: ', mail.subject);
+        console.log('mail.messageId: ', mail.messageId);
+        //console.log('mail.text: ', mail?.text) 
+        console.log('id/attributes.uid : ', id);
     });
+    
+   // await connection.moveMessage(messageId, 'Рабочие');
 
     connection.end();
 
 //source:
 //https://github.com/chadxz/imap-simple
 //https://github.com/mscdex/node-imap
+
+//TODO:
+// uninstall lodash
