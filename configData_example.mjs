d4b9eb8bebe686f320329c14d98a77673f955af5
Svetlana Markova <80004406+marss-hub@users.noreply.github.com>
2025-02-03 18:50:01 +0500

    Add config with example data

---
+++ configData_example.mjs
@@ -0,0 +1,7 @@
+export const CONFIGDATA = {
+  host: "{imap.yourserver.ru}", // server host
+  port: 999, // port
+  user: "{username}", // example username
+  pass: "{xxxxxxxxxxxx}", // example password
+  secure: true,
+};
